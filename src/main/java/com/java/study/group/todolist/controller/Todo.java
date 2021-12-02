38c682d971d7d3132e9bb7bf515cc51f12a333e4
Joyce <joyce.apsmartins@gmail.com>
2021-12-02 20:46:53 +0000

    Adding task

--- src/main/java/com/java/study/group/todolist/controller/Todo.java
+++ src/main/java/com/java/study/group/todolist/controller/Todo.java
@@ -6,18 +6,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/todo")
 public class Todo {
     @Autowired
     private TaskRepository taskRepository;
 
-    @GetMapping("/todo")
+    @GetMapping
     public String todo (Model model) {
         List <Task> tasks = taskRepository.findAll();
         model.addAttribute("tasks", tasks);
         return "todo";
+    }
+
+    @PostMapping
+    public ModelAndView addTask (Task task) {
+        ModelAndView modelAndView = new ModelAndView("todo");
+        taskRepository.save(task);
+
+        List <Task> tasks = taskRepository.findAll();
+        modelAndView.addObject("tasks",tasks);
+
+        return modelAndView;
     }
 }
